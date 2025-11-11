d45115b4576f39cf5e7753829e1d6a029572e88d
stefandinca <stefan.dinca07@gmail.com>
2025-11-11 16:28:54 +0200

    initial-commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['Satoshi', 'sans-serif'],
+      },
+      screens: {
+        '3xl': '120rem',
+      },
+      colors: {
+        avocado: {
+          100: 'oklch(0.99 0 0)',
+          200: 'oklch(0.98 0.04 113.22)',
+          300: 'oklch(0.94 0.11 115.03)',
+          400: 'oklch(0.92 0.19 114.08)',
+          500: 'oklch(0.84 0.18 117.33)',
+          600: 'oklch(0.53 0.12 118.34)',
+        },
+      },
+    },
+  },
+};
